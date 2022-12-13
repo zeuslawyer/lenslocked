ce473a3c7a1526ef1454ec2ff89aaa3e3c6e7fdf
Zubin Pratap <zubin.pratap@gmail.com>
2022-12-13 16:29:48 +1100

    add 404 handler and various http package tricks

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Background
+# Background / Motivation
 
 Learning web dev with Golang.
 
 Lenslocked and other projects are just things I've played around with to build up my mental models.
+
+# Tooling
+
+- golang
+- github.com/cortesi/modd for dynamic reloading through detecting code/filesystem changes. Installed using `brew install modd`. Config is in the `modd.conf` file in project root.
+
--- main.go
+++ main.go
@@ -5,12 +5,38 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Contact Me Here</h1>")
+}
+
+func noResource404Handler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h1>Error: %s</h1>", http.StatusText(http.StatusNotFound))
+	fmt.Fprintf(w, "<h1>404: No resource found at %q </h1>", r.URL.Path)
+	fmt.Fprintf(w, "<h1>404: No resource found at %q </h1>", r.URL.RawPath)
+}
+
+func routeHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	switch path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// http.NotFound(w, r)
+		// http.Error(w, fmt.Sprintf("Hmmm...resource at path %q was not found.", r.URL.Path), http.StatusNotFound)
+		noResource404Handler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", routeHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }

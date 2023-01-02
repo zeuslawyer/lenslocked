e438c7ee5d5c199857845b0d0072eefc127614ba
Zubin Pratap <zubin.pratap@gmail.com>
2023-01-02 12:05:46 +0530

    add chi router

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,54 +40,21 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	`)
 }
 
-func noResource404Handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "<h1>Error: %s</h1>", http.StatusText(http.StatusNotFound))
-	fmt.Fprintf(w, "<h1>404: No resource found at %q </h1>", r.URL.Path)
-	fmt.Fprintf(w, "<h1>404: No resource found at %q </h1>", r.URL.RawPath)
-}
-
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	path := r.URL.Path
-// 	switch path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		// http.NotFound(w, r)
-// 		// http.Error(w, fmt.Sprintf("Hmmm...resource at path %q was not found.", r.URL.Path), http.StatusNotFound)
-// 		noResource404Handler(w, r)
-// 	}
-// }
-
-// Router implement the http.Handler interface.
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	switch path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		// http.NotFound(w, r)
-		// http.Error(w, fmt.Sprintf("Hmmm...resource at path %q was not found.", r.URL.Path), http.StatusNotFound)
-		noResource404Handler(w, r)
-	}
-}
-
+// Using Chi.
 func main() {
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-	// http.HandleFunc("/", pathHandler)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// Routes
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	// r.NotFound(http.NotFound)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, fmt.Sprintf("Hmmm...resource at path %q was not found :(", r.URL.Path), http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
 
-	var router Router // http.HandlerFunc = pathHandler
-	http.ListenAndServe(":3000", router)
-	// http.ListenAndServe(":3000", nil)
 }
